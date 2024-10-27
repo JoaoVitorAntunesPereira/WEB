bee6b8c3e0e9236c49cfd1c8f1acab6ea131e5ec
João Vitor <114810543+JoaoVitorAntunesPereira@users.noreply.github.com>
2024-10-27 13:42:01 -0300

    att

--- WEB-2/aula-5/atividade-progressao-aritmetica.php
+++ WEB-2/aula-5/atividade-progressao-aritmetica.php
@@ -5,16 +5,16 @@ $razao;
 $quantidade;
 
 if(isset($_GET['inicio']) != 1){
-  echo "Informe o inicio da progressão!";
+  echo "Informe o inicio da progressão![inicio='']";
 }else 
   if(isset($_GET['razao']) != 1){
-    echo "Informe a razão da progressão!";
-    $inicio = $_GET['inicio'];
+    echo "Informe a razão da progressão![razao='']";
 }else
   if(isset($_GET['quantidade']) != 1){
-    echo "Informe a quantidade de elementos da progressão!";   
-    $razao = $_GET['razao']; 
+    echo "Informe a quantidade de elementos da progressão![quantidade='']";   
 }else{
+  $inicio = $_GET['inicio'];
+  $razao = $_GET['razao']; 
     $quantidade = $_GET['quantidade'];
     construirProgressao($inicio, $razao, $quantidade);
 }
