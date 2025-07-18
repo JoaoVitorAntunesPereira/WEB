c1551dbf0e6cbf246e022f8a68d6e5e833db9fbd
João Vitor <114810543+JoaoVitorAntunesPereira@users.noreply.github.com>
2025-07-18 17:17:55 -0300

    att

--- WEB-3/ordem-de-servico-api/src/main/java/com/spring/projects/ordem_de_servico_api/models/StatusOrdemServico.java
+++
@@ -1,7 +0,0 @@
-package com.spring.projects.ordem_de_servico_api.models;
-
-public enum StatusOrdemServico {
-    ABERTA,
-    FINALIZADA, 
-    CANCELADA
-}
--- WEB-3/ordem-de-servico-api/src/main/java/com/spring/projects/ordem_de_servico_api/repositories/ComentarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.projects.ordem_de_servico_api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.projects.ordem_de_servico_api.models.Comentario;
-
-public interface ComentarioRepository extends JpaRepository<Comentario, Long>{
-    
-}
